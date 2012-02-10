a3032c1038cf5bb417e6034ada4b27a1eb2b0986
Daniel Ashby <ashbygeek@gmail.com>
2012-02-10 15:27:04 -0500

    cartoon squirrels, ghost squirrel AI

--- Assets/Squirrel Assets/squirrelAI_ghost.cs
+++ Assets/Squirrel Assets/squirrelAI_ghost.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 using System.Collections;
 
-public class squirrelAI : MonoBehaviour {
+public class squirrelAI_ghost : MonoBehaviour {
 	private GameObject[] characters;
 	
-	public float moveSpeed = 1.0f;
+	public float moveSpeed = 0.2f;
+	public float minDist = 1.0f;
 	
 	// Use this for initialization
 	void Start () {
@@ -17,14 +18,15 @@ public class squirrelAI : MonoBehaviour {
 		float closestDist = 99999999999999999999999.0f;
 		foreach(GameObject tmp in characters){
 			Vector3 dif = tmp.transform.position - transform.position;
-			if (dif.sqrMagnitude < closestDist){
+			if (dif.sqrMagnitude < closestDist && dif.sqrMagnitude > minDist){
 				closestDist = dif.sqrMagnitude;
 				closest = tmp;
 			}
 		}
 		
 		if (closest != null){
-			rigidbody.velocity = Vector3.forward * moveSpeed;
+			transform.LookAt(closest.transform.position,-Vector3.up);
+			transform.position += transform.forward * moveSpeed;
 		}
 	}
 }
